affd235e4ab1b29e0a58e3c1c07bde1054a5a61f
Alex Walsh <akiregator@gmail.com>
2018-02-07 16:44:59 -0800

    First Backend Commit

---
+++ app.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var validator = require('express-validator')
+var app = express();
+const { Available } = require('./models')
+const { User } = require('./models')
+
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(validator())
+
+app.get('/', (req, res) => {
+	res.json({message: 'API Example App'})
+});
+
+app.get('/availabilities', (req, res) => {
+	Available.findAll().then( (availabilities) =>{
+		res.json({availabilities:availabilities})
+	})
+})
+
+
+app.post('/availabilities', (req, res) => {
+		req.checkBody('loa', 'Is required').notEmpty()
+		req.checkBody('kind', 'Is required').notEmpty()
+		req.checkBody('location', 'Is required').notEmpty()
+		req.checkBody('description', 'Is required').notEmpty()
+
+	req.getValidationResult()
+	  .then((validationErrors) =>{
+		if(validationErrors.isEmpty()){
+	  Available.create({
+	    loa: req.body.loa,
+	    kind: req.body.kind,
+	    location: req.body.location,
+		description: req.body.description
+	}).then((available)=>{
+	    res.status(201)
+	    res.json({available: available})
+	 })
+	  	}else{
+		res.status(400)
+		res.json({errors: {validations: validationErrors.array()}})
+	  }
+	})
+})
+
+//Begins 'get' and 'post' process for /users route path.
+
+app.get('/users', (req, res) => {
+	Available.findAll().then( (users) =>{
+		res.json({users:users})
+	})
+})
+
+
+app.post('/users', (req, res) => {
+		req.checkBody('firstname', 'Is required').notEmpty()
+		req.checkBody('lastname', 'Is required').notEmpty()
+		req.checkBody('email', 'Is required').notEmpty()
+		req.checkBody('password', 'Is required').notEmpty()
+		req.checkBody('phone', 'Is required').notEmpty()
+
+	req.getValidationResult()
+	  .then((validationErrors) =>{
+		if(validationErrors.isEmpty()){
+	  User.create({
+	    firstname: req.body.firstname,
+	    lastname: req.body.lastname,
+	    email: req.body.email,
+		password: req.body.password,
+		phone: req.body.phone
+	}).then((user)=>{
+	    res.status(201)
+	    res.json({user: user})
+	 })
+	  	}else{
+		res.status(400)
+		res.json({errors: {validations: validationErrors.array()}})
+	  }
+	})
+})
+
+
+
+module.exports = app
---
+++ migrations/20180206220938-create-available.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Availabilities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      loa:{
+		  type: Sequelize.INTEGER
+      },
+	  kind:{
+		  type: Sequelize.STRING
+	  },
+      type: {
+        type: Sequelize.STRING
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Availabilities');
+  }
+};
