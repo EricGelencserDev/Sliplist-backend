4e7df7ba7c90284de6d0b23118b985a8f8d861ef
ScotthFarris <akiregator@gmail.com>
2018-02-22 15:39:23 -0800

    fix poert

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT || 3001
+const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : 3001
 
 app.listen(PORT, () => {
 	console.log("Listening on:", PORT)
